a353eb90ea4b98d7548a5a3b1fc69f43a99abacb
Varshini.R <97427178+VarshiniRajkumar@users.noreply.github.com>
2025-06-22 14:04:35 +0530

    Add files via upload

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("Browser Version:" +version+ "\n" +"Developer: " +developer+ "\n" +"IsBeta: " +isBeta+ "\n" +"Release Year: " +releaseYear+ "\n" +"Shortcut Key for broweser is: " +shortcutKey);
+
+	}
+}
---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) { // entry point of the program
+		System.out.println("welcome to testleaf");
+
+	}
+}
